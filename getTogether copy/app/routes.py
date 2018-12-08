8d8865d8c0d3338c4638dfd0e722747a227dd3f9
jmwashi <31834822+jmwashi@users.noreply.github.com>
2018-12-08 16:00:10 -0500

    Add files via upload

--- getTogether copy/app/routes.py
+++ getTogether copy/app/routes.py
@@ -20,35 +20,51 @@ def index():
         return redirect('/authorize/facebook')
     return render_template("index.html", title='Home Page')
 
-@app.route('/loggedout')
-def loggedout():
-    form = EventCreateForm()
-    return render_template('createEvent.html',form=form)
+
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('loggedout'))
+    return redirect(url_for('index'))
 
 #need to call database to grab current user to properly create event and meetup. doing tonight
-@app.route('/create_event',methods=['GET', 'POST'])
-def createEvent():
+@app.route('/create_event/<meet_id>',methods=['GET', 'POST'])
+@login_required
+def createEvent(meet_id):
     form = EventCreateForm()
     if form.validate_on_submit():
-        event = Event(title = form.title.data, description = form.description.data, address = form.address.data, city = form.city.data,time = form.time.data)
+        event = Event(title = form.title.data,this_id=meet_id, description = form.description.data, address = form.address.data, city = form.city.data,time = form.time.data)
         db.session.add(event)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('meetups'))
     return render_template('createEvent.html',form=form)
-#same as above
+
+@app.route('/meet/<meet_id>',methods=['GET','POST'])
+@login_required
+def thisMeet(meet_id):
+    user = User.query.filter_by(id=current_user.id).first_or_404()
+    meet = Meet.query.filter_by(id=meet_id).first_or_404()
+    events = Event.query.filter_by(this_id=meet.id).all()
+    return render_template('thisMeet.html',user=user,meet=meet,events=events)
+
+
 @app.route('/create_meetup',methods=['GET', 'POST'])
+@login_required
 def createMeetUp():
     form = MeetUpCreateForm()
     if form.validate_on_submit():
-        meet_up = Meet(title = form.title.data, description = form.description.data, city = form.city.data)
+        meet_up = Meet(title = form.title.data,owner_id = current_user.id, description = form.description.data, city = form.city.data)
         db.session.add(meet_up)
         db.session.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('meetups'))
     return render_template('createMeetUp.html',form=form)
+
+@app.route('/meetups')
+@login_required
+def meetups():
+    user = User.query.filter_by(id=current_user.id).first_or_404()
+    meet_ups = Meet.query.filter_by(owner_id=current_user.id).all()
+    return render_template('meetups.html',user=user,meet_ups=meet_ups)
 
 @app.route('/authorize/<provider>')
 def oauth_authorize(provider):
@@ -61,7 +77,8 @@ def oauth_authorize(provider):
 @app.route('/callback/<provider>')
 def oauth_callback(provider):
     if not current_user.is_anonymous:
-        return redirect(url_for('loggedout'))
+        print('test')
+        return redirect(url_for('index'))
     oauth = OAuthSignIn.get_provider(provider)
     social_id, username, email = oauth.callback()
     if social_id is None:
