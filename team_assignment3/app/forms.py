f49121a6d6ae4f895685520e7c7c569baaecd771
Kaijie Zhou <kaijiez@bu.edu>
2018-12-09 15:53:48 -0500

    eventbrite

--- team_assignment3/app/forms.py
+++ team_assignment3/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField ,RadioField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
@@ -7,3 +7,13 @@ class SearchForm(FlaskForm):
     city = StringField('Location', validators=[DataRequired()])
     numberOfRestaruants = IntegerField('Number of Searches', validators=[DataRequired()])
     submit = SubmitField('Search')
+
+class SearchEventForm(FlaskForm):
+	keyword = StringField('Keyword', validators=[DataRequired()])
+	sort_by = RadioField("Sort_by",choices=[("date","date"),("distance","distance"),("best","best")],validators=[DataRequired()])
+	location = StringField("Location",validators=[DataRequired()])
+	location_within = IntegerField ("Location_within_km",validators=[DataRequired()])
+	price = RadioField("price",choices=[("free","free"),("paid","paid")],validators=[DataRequired()])
+	start_after = StringField("Start_after (YYYY-MM-DD HH:MM:SS)", validators=[DataRequired()])
+	start_before = StringField("Start_before (YYYY-MM-DD HH:MM:SS)", validators=[DataRequired()])
+	submit = SubmitField('Search')
--- team_assignment3/app/routes.py
+++ team_assignment3/app/routes.py
@@ -1,7 +1,19 @@
 from app import app
 import requests
-from flask import render_template, flash, redirect
-from app.forms import SearchForm
+from flask import render_template, flash, redirect,url_for , Flask
+from app.forms import SearchForm,SearchEventForm
+
+#from eventbrite import Eventbrite
+import requests
+import json
+import pytz, datetime
+
+def local_to_utc(time):
+    local = pytz.timezone("America/Los_Angeles")
+    naive = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    local_dt = local.localize(naive, is_dst=None)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    return utc_dt.strftime ("%Y-%m-%dT%H:%M:%SZ")
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -26,6 +38,50 @@ def index():
         return render_template("response.html",title='Title',result=finalString)
     return render_template('index.html', title='Title', form=form)
 
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_event():
+  form = SearchEventForm()
+  if form.validate_on_submit():
+    # input 
+    params={
+        "q": form.keyword.data,
+        "sort_by":form.sort_by.data,
+        "location.address":form.location.data,
+        "location.within": str(form.location_within.data)+"km",
+        "price":form.price.data,
+        "start_date.range_start":local_to_utc(form.start_after.data),
+        "start_date.range_end": local_to_utc(form.start_before.data),
+        #"start_date.keyword":""
+    }
+    search_string=""
+    for param in params:
+        search_string+=str(param)+"="+str(params[param])+"&"
+
+    # parse the result
+    result=requests.get("https://www.eventbriteapi.com/v3/events/search/?"+search_string+"&token=SYL7IV644GMFGG7FON47")
+    result=result.json()
+    display_string=[]
+    for event in result["events"]:
+
+        new_event={"name":event["name"]["text"],
+             "description":event["description"]["text"],
+             "id":event["id"],
+             "start_time":event["start"]["local"],
+             "end_time":event["end"]["local"],
+             "online":event["online_event"],
+             "is_free":event["is_free"],
+             "logo":"",
+             "url":event["url"]
+             }
+
+        if event["logo"] is not None:
+           new_event["logo"]=event["logo"]["url"]
+
+        display_string.append(new_event)
+            
+    return render_template("eventbrite.html", form=form, result=display_string)
+  return render_template("eventbriteform.html",form=form)
 
 #@app.route('/result')
 #def result():
